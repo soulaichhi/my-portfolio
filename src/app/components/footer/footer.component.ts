d3be8c695339527374f7e33b8cf3d46c9c1c337a
soulaichhi <chhibou.soulaimane@gmail.com>
2023-03-15 12:18:43 +0100

    Create the Footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  openLinkedin() {
+    window.open('https://www.linkedin.com/in/soulaimane-chhibou/');
+  }
+  openGithub() {
+    window.open('https://github.com/soulaichhi');
+  }
 }
